22dd3f60ff46747341576ea8747a3e1e385af85d
Tim Yates <tim.yates@gmail.com>
2022-12-03 18:54:28 +0000

    Fix loading of input file

--- src/main/groovy/Day1.groovy
+++ src/main/groovy/Day1.groovy
@@ -2,7 +2,7 @@ def elves = [:]
 def currentFood = 0
 def currentElf = 0
 
-new File('src/main/resources/input/day-1-small.txt').eachLine { line ->
+this.getClass().getResourceAsStream("/input/day-1-small.txt").eachLine { line ->
     if (line.isBlank()) {
         elves[currentElf++] = currentFood
         currentFood = 0
